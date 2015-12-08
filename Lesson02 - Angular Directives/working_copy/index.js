87eba6f3f7366429e16e9eb0621233518c4ba52a
Charlye Tran <tran1226@yahoo.com>
2015-12-07 21:35:23 -0500

    updated index for week 2

--- Lesson02 - Angular Directives/working_copy/index.js
+++ Lesson02 - Angular Directives/working_copy/index.js
@@ -1,4 +1,30 @@
 var myApp = angular.module('myApp', []).controller('MainCtrl', function(){
   var self = this;
+  
   self.greeting = "Hello World";
+  
+  self.onUserClick = function() {
+  	alert('User Clicked!');
+  };
+
+  self.simpleArray = ['Item 1', 'Item 2', 'Item 3'];
+
+  self.intermediateArray = [{name:'George', age: 13}, {name: 'Bill', age:13}, {name: 'Jake', age: 5}]
+
+  self.myDog = {
+  	age: 12,
+  	name: 'Harry',
+  	color: 'black'
+  }
+  self.dog = {
+    name: 'Max'
+  };
+
+  self.cat = {
+    name: 'Sally'
+  };
+
+  self.hamster = {
+    name: 'Gerbal'
+  };
 });
