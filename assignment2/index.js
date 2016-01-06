4deaa268823cf59ad6c54a1ae627abefeebddcac
Charlye Tran <tran1226@yahoo.com>
2016-01-06 18:19:29 -0500

    updated assignment 2

--- assignment2/index.js
+++ assignment2/index.js
@@ -2,4 +2,8 @@ angular.module('Simon', [])
 
 .controller('MainCtrl', function(){
 	self = this;
+	this.litUp = false;
+	self.startSimon = function(){
+		console.log("You are starting a Simon Game!!");
+	}
 })
