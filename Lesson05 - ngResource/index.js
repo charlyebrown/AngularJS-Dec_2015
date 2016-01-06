293e20e7754b1a091117cf20128a1c4b0a8ec6e1
Ehtesh Choudhury <ehtesh.choudhury@gmail.com>
2016-01-06 18:18:27 -0500

    ngResource part2

--- Lesson05 - ngResource/index.js
+++ Lesson05 - ngResource/index.js
@@ -1,17 +1,44 @@
 
 angular.module('myApp', ['MyServicesModule'])
 
-.controller('MainCtrl', function(MyResource) {
+.controller('MainCtrl', function(MyResource, FriendsResource) {
 	var self = this;
 
-	MyResource.getPeople({
+	// MyResource.getPeople({
+	// 	page: 2,
+	// 	age: 30
+	// })
+	// .$promise
+	// .then(function onSuccess(response) {
+	// 	self.people = response;
+	// }, function onError(error) {
+
+	// });
+
+	MyResource.query({
 		page: 2,
 		age: 30
-	})
-	.$promise
-	.then(function onSuccess(response) {
+	}, function onSuccess(response) {
 		self.people = response;
 	}, function onError(error) {
 
 	});
+
+	self.displayFriends = function(person) {
+		FriendsResource.query({
+			personId: person._id
+		}).$promise
+		.then(function onSuccess(response) {
+			person.friends = response;
+
+			// Chaining promises example
+			//
+			// return FriendsResource.query({
+			// 	personId: self.people[2]._id
+			// }).$promise;
+		})
+		// .then(function onSuccess(response) {
+		// 	self.people[2].friends = response;
+		// });
+	};
 });
